026e05d630fe0eefc9863c6f28165afdd54ad1be
Abeer Salameh <abeer.salameh@gmail.com>
2016-03-15 04:47:12 +0000

    Add page (node)
    Also add to_s method to output to file

---
+++ lib/crawler/page.rb
@@ -0,0 +1,26 @@
+class Crawler
+  class Page
+    attr_accessor :url, :js_assets, :img_assets, :css_assets, :children
+
+    def initialize(url:, assets:)
+      @url = url
+      @js_assets = assets.fetch(:js_assets)
+      @img_assets = assets.fetch(:img_assets)
+      @css_assets = assets.fetch(:css_assets)
+    end
+
+    def to_s # rubocop:disable Metrics/MethodLength
+      "---------------------------------------\n" \
+      "Page: #{url}\n" \
+      "---------------------------------------\n" \
+      "assets:\n" \
+      "javascript:\n" \
+      "#{js_assets.join("\n")}" \
+      "images:\n" \
+      "#{img_assets.join("\n")}" \
+      "stylesheets:\n" \
+      "#{css_assets.join("\n")}" \
+      "\n"
+    end
+  end
+end
---
+++ spec/crawler/page_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+RSpec.describe Crawler::Page do
+  subject do
+    described_class.new(
+      url: URI.parse("https://gocardless.com"),
+      assets: {
+        js_assets: [URI.parse("www.googletagmanager.com/gtm.js")],
+        img_assets: [URI.parse("https://gocardless.com/images/logos/funding.png")],
+        css_assets: [URI.parse("https://gocardless.com/bundle/main.css")]
+      }
+   )
+  end
+
+  let(:expected_output) do
+    "---------------------------------------\n" \
+    "Page: https://gocardless.com\n" \
+    "---------------------------------------\n" \
+    "assets:\n" \
+    "javascript:\n" \
+    "www.googletagmanager.com/gtm.js" \
+    "images:\n" \
+    "https://gocardless.com/images/logos/funding.png" \
+    "stylesheets:\n" \
+    "https://gocardless.com/bundle/main.css" \
+    "\n"
+  end
+
+  it "sets the correct string output" do
+    expect(subject.to_s).to eq(expected_output)
+  end
+end
